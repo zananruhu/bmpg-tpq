f4e470a392123ea9a9d09a457812df61e4a787c7
zanan <zananruhu@gmail.com>
2022-08-12 03:16:34 +0700

    test4

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -6,7 +6,7 @@
       <li class="nav-item active">
           <a class="nav-link" href="/">
               <i class="fas fa-fw fa-tachometer-alt"></i>
-              <span class="mt-auto">BMPG-TPQ=</span></a>
+              <span class="mt-auto">BMPG-TPQ</span></a>
       </li>
 
       <!-- Divider -->
